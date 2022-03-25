89f73d302d245aaba7cd99bc9312d14b13db8451
cubecubb <annazibereva999@gmail.com>
2022-03-25 12:01:06 +0700

    git commit "изменение комментария в ExampleUnitTest.java"

--- app/src/test/java/com/example/projectgitzibereva/ExampleUnitTest.java
+++ app/src/test/java/com/example/projectgitzibereva/ExampleUnitTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {//ыррто
+public class ExampleUnitTest {//ыррррр
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
